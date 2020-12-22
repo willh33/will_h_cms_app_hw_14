0bb6c80206e3aab89d5b915db27b39568b78184b
willh33 <wharrison33@hotmail.com>
2020-12-21 20:56:46 -0700

    fixed format date function, it was adding 5 years

--- utils/helpers.js
+++ utils/helpers.js
@@ -16,7 +16,7 @@ module.exports = {
   },
   format_date: (date) => {
     return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
-      new Date(date).getFullYear() + 5
+      new Date(date).getFullYear()
     }`;
   },
   stringify: (post) => JSON.stringify(post),
